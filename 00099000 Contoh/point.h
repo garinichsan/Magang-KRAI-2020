e1a495d9c9dc39def31e7316111853d8de5ad73d
94gin <garin.kra@gmail.com>
2020-02-24 22:35:03 +0700

    Contoh

---
+++ 00099000 Contoh/point.h
@@ -0,0 +1,52 @@
+/* File: point.h */
+/* *** Definisi ABSTRACT DATA TYPE POINT *** */
+
+#ifndef POINT_H
+#define POINT_H
+
+typedef struct { 
+	float X; /* absis   */
+	float Y; /* ordinat */
+} POINT;
+
+/* *** Notasi Akses: Selektor POINT *** */
+#define Absis(P) (P).X
+#define Ordinat(P) (P).Y
+        
+/* *** DEFINISI PROTOTIPE PRIMITIF *** */
+/* *** Konstruktor membentuk POINT *** */
+POINT MakePOINT (float X, float Y);
+/* Membentuk sebuah POINT dari komponen-komponennya */
+
+/* *** KELOMPOK Interaksi dengan I/O device, BACA/TULIS  *** */                                                 
+void BacaPOINT (POINT * P); 
+/* Membaca nilai absis dan ordinat dari keyboard dan membentuk 
+   POINT P berdasarkan dari nilai absis dan ordinat tersebut */
+/* Komponen X dan Y dibaca dalam 1 baris, dipisahkan 1 buah spasi */
+/* Contoh: 1 2 
+   akan membentuk POINT <1,2> */
+/* I.S. Sembarang */
+/* F.S. P terdefinisi */
+void TulisPOINT (POINT P);
+/* Nilai P ditulis ke layar dengan format "(X,Y)" 
+   tanpa spasi, enter, atau karakter lain di depan, belakang, 
+   atau di antaranya */
+/* I.S. P terdefinisi */
+/* F.S. P tertulis di layar dengan format "(X,Y)" */                
+
+/* *** Kelompok operasi relasional terhadap POINT *** */
+bool EQ (POINT P1, POINT P2);
+/* Mengirimkan true jika P1 = P2 : absis dan ordinatnya sama */
+int Kuadran (POINT P);
+/* Menghasilkan kuadran dari P: 1, 2, 3, atau 4 */
+/* Prekondisi : P bukan titik origin, */
+/*              dan P tidak terletak di salah satu sumbu */
+
+/* *** KELOMPOK OPERASI LAIN TERHADAP TYPE *** */
+void Geser (POINT *P, float deltaX, float deltaY);
+/* I.S. P terdefinisi */
+/* F.S. P digeser, absisnya sebesar deltaX dan ordinatnya sebesar deltaY */
+float Jarak0 (POINT P);
+/* Menghitung jarak P ke (0,0) */
+
+#endif
